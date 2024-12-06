bf6f7a0fe190a52c956cfa74dba90bfb87b17f5c
waspoza <piotr@amnezja.pl>
2024-12-06 17:08:57 +0100

    atomic arena and stack added

---
+++ src/atomic.rs
@@ -0,0 +1,75 @@
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+#[derive(Debug)]
+pub struct Arena {
+    buf: Vec<u8>,
+    idx: AtomicUsize,
+}
+unsafe impl Send for Arena {}
+unsafe impl Sync for Arena {}
+impl Arena {
+    pub fn new(capacity: usize) -> Self {
+        Arena {
+            buf: Vec::<u8>::with_capacity(capacity),
+            idx: AtomicUsize::new(0),
+        }
+    }
+    pub fn alloc(&self, size: usize) -> *const u8 {
+        // round up size to be divisible by 8
+        // to avoid misalignings
+        let new_size;
+        if size % 8 > 0 {
+            new_size = size + 8 - (size % 8);
+        } else {
+            new_size = size;
+        }
+        let old = self.idx.fetch_add(new_size, Ordering::Relaxed);
+        assert!(
+            old + size <= self.buf.capacity(),
+            "alloc size exceeded buf capacity"
+        );
+        unsafe { self.ptr().add(old) }
+    }
+    #[allow(dead_code)]
+    pub fn write(&self, offset: usize, val: u8) {
+        unsafe {
+            let ptr = self.ptr().add(offset) as *mut u8;
+            ptr.write(val);
+        }
+    }
+    #[inline]
+    fn ptr(&self) -> *const u8 {
+        self.buf.as_ptr()
+    }
+}
+
+//#[derive(Debug)]
+pub struct Stack<T> {
+    buf: Vec<T>,
+    idx: AtomicUsize,
+}
+unsafe impl<T> Send for Stack<T> {}
+unsafe impl<T> Sync for Stack<T> {}
+impl<T> Stack<T> {
+    pub fn new(capacity: usize) -> Self {
+        Stack {
+            buf: Vec::<T>::with_capacity(capacity),
+            idx: AtomicUsize::new(0),
+        }
+    }
+    pub fn push(&self, item: T) {
+        let old_idx = self.idx.fetch_add(1, Ordering::Relaxed);
+        assert!(old_idx + 1 <= self.buf.capacity(), "stack full");
+        unsafe {
+            let ptr = self.buf.as_ptr().add(old_idx) as *mut T;
+            ptr.write(item);
+        }
+    }
+    pub fn into_vec(self) -> Vec<T> {
+        let me = std::mem::ManuallyDrop::new(self);
+        let len = me.idx.load(Ordering::Relaxed);
+        let cap = me.buf.capacity();
+        let ptr = me.buf.as_ptr() as *mut T;
+        unsafe { Vec::from_raw_parts(ptr, len, cap) }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ use std::ffi::{c_char, CString};
 use std::io;
 use std::os::unix::ffi::OsStrExt;
 use std::path::PathBuf;
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Arc;
 //use rayon_core;
+
+mod atomic;
 
 macro_rules! try_posix_fn {
     ($call:expr) => {
@@ -22,43 +24,20 @@ macro_rules! try_posix_fn {
         }
     };
 }
+
 #[derive(Debug)]
-struct Arena {
-    buf: Vec<u8>,
-    idx: AtomicUsize,
-}
-unsafe impl Send for Arena {}
-unsafe impl Sync for Arena {}
-impl Arena {
-    fn new(capacity: usize) -> Self {
-        Arena {
-            buf: Vec::<u8>::with_capacity(capacity),
-            idx: AtomicUsize::new(0),
-        }
-    }
-    fn alloc(&self, size: usize) -> *const u8 {
-        let old = self.idx.fetch_add(1, Ordering::Relaxed);
-        if old + size > self.buf.capacity() {
-            panic!("alloc size exceeded buf capacity")
-        }
-        unsafe { self.get().add(old) }
-    }
-    fn write(&self, offset: usize, val: u8) {
-        unsafe {
-            let ptr = self.get().add(offset) as *mut u8;
-            *ptr = val;
-        }
-    }
-    fn get(&self) -> *const u8 {
-        self.buf.as_ptr()
-    }
+struct Entry {
+    dir: Arc<PathBuf>,
+    name: &'static str,
+    date: usize,
 }
 
 fn main() -> Result<(), std::io::Error> {
+    let arena = atomic::Arena::new(8_000_000);
+    let stack = atomic::Stack::<Entry>::new(101);
+    let mut dirents = vec![];
 
-    let arena = Arena::new(8_000_000);
-
-    let path = PathBuf::from("/home/piotr");
+    let path = Arc::new(PathBuf::from("/home/piotr"));
     let dirfd = unsafe {
         let path = CString::new(path.as_os_str().as_bytes())?;
         try_posix_fn!(libc::open(
@@ -67,37 +46,79 @@ fn main() -> Result<(), std::io::Error> {
         ))
     };
 
-    let ptr = arena.alloc(1024);
-    let res = unsafe { libc::syscall(libc::SYS_getdents64, dirfd, ptr, 1024) };
-    let dirent: *const libc::dirent64 = ptr.cast();
+    let buf_size = 1024;
+    loop {
+        let ptr = arena.alloc(buf_size);
+        let mut idx = 0;
 
-    let d_reclen = unsafe { (*dirent).d_reclen as usize };
+        let bytes_read = loop {
+            let res = unsafe { libc::syscall(libc::SYS_getdents64, dirfd, ptr, buf_size) };
 
-    //let name = unsafe { CStr::from_ptr((*dirent).d_name.as_ptr()) };
-    let d_name: *const c_char = unsafe { (*dirent).d_name.as_ptr() };
-    let namelen = unsafe { libc::strlen(d_name) };
-    let name: &[u8] = unsafe { std::slice::from_raw_parts(d_name.cast(), namelen) };
-    let n = unsafe { std::str::from_utf8_unchecked(name) };
+            if res >= 0 {
+                break res as usize;
+            }
 
-    dbg!(ptr);
-    dbg!(dirfd);
-    dbg!(res);
-    dbg!(d_reclen);
-    dbg!(name);
-    println!("{}", n);
+            // `res` contains an error. Retry if it is `EINTR`.
+            let error = -res as i32;
+            if error != libc::EINTR {
+                return Err(io::Error::from_raw_os_error(error));
+            }
+        };
+        dbg!(bytes_read);
+        if bytes_read == 0 {
+            break; // no more entries
+        }
+        loop {
+            let dirent: *const libc::dirent64 = unsafe { ptr.add(idx).cast() };
 
-    (0..100).into_par_iter().for_each(|x| {
+            let d_reclen = unsafe { (*dirent).d_reclen as usize };
+            //let name = unsafe { CStr::from_ptr((*dirent).d_name.as_ptr()) };
+            let d_name: *const c_char = unsafe { (*dirent).d_name.as_ptr() };
+            let namelen = unsafe { libc::strlen(d_name) };
+            let slice: &[u8] = unsafe { std::slice::from_raw_parts(d_name.cast(), namelen) };
+            let name = unsafe { std::str::from_utf8_unchecked(slice) };
+
+            let entry = Entry {
+                dir: path.clone(),
+                name,
+                date: 0,
+            };
+            dbg!(&entry);
+            dirents.push(entry);
+
+            idx += d_reclen;
+            dbg!(idx);
+            if idx >= bytes_read {
+                break;
+            }
+        }
+    }
+
+    //dbg!(ptr);
+    //dbg!(dirfd);
+    //dbg!(res);
+    //dbg!(d_reclen);
+    //dbg!(name);
+    //dbg!(n);
+
+    dirents.into_par_iter().for_each(|x| {
         //let old = idx.fetch_add(1, Ordering::Relaxed);
         //let a = aptr.load(Ordering::Relaxed);
         //unsafe {
         //    *a.add(old) = old as u8;
         //}
 
-        arena.write(x, x as u8);
+        //arena.write(x + 1024, x as u8);
+
+        //dbg!(&entry);
+        stack.push(x);
         //dbg!(&sptr.get());
     });
 
-    dbg!(unsafe { *ptr.add(66) });
+    //dbg!(unsafe { *ptr.add(66) });
+    let v = stack.into_vec();
+    dbg!(v.len(), v.capacity());
+    dbg!(v);
 
     Ok(())
 }
