9b50d8fef1ab77642df436beba5e3683607ed262
waspoza <waspoza@gmail.com>
2026-05-17 14:47:46 +0200

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Search for text inside emails";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "e";
+          version = "0.1.0";
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          meta = with pkgs.lib; {
+            description = "Search for text inside emails";
+            mainProgram = "e";
+            license = licenses.bsd3;
+            platforms = platforms.linux;
+          };
+        };
+      }
+    );
+}
